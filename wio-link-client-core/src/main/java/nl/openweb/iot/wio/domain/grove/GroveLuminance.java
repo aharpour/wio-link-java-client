1979ffe9458156641bf9f5f045d5a01976d6cc88
Ebrahim aharpour <ebrahim@openweb.nl>
2017-01-23 21:19:21 +0100

    more sensors were added.

---
+++ wio-link-client-core/src/main/java/nl/openweb/iot/wio/domain/grove/GroveLuminance.java
@@ -0,0 +1,19 @@
+package nl.openweb.iot.wio.domain.grove;
+
+import nl.openweb.iot.wio.WioException;
+import nl.openweb.iot.wio.db.GroveBean;
+import nl.openweb.iot.wio.domain.Grove;
+import nl.openweb.iot.wio.domain.Node;
+import nl.openweb.iot.wio.domain.Type;
+import nl.openweb.iot.wio.rest.NodeResource;
+
+@Type
+public class GroveLuminance extends Grove {
+    public GroveLuminance(GroveBean groveBean, Node parent, NodeResource nodeResource) {
+        super(groveBean, parent, nodeResource);
+    }
+
+    public Double readLuminance() throws WioException {
+        return toDouble(readSimpleProperty("luminance", "lux"));
+    }
+}
---
+++ wio-link-client-core/src/main/java/nl/openweb/iot/wio/domain/grove/GroveUltraRanger.java
@@ -0,0 +1,23 @@
+package nl.openweb.iot.wio.domain.grove;
+
+import nl.openweb.iot.wio.WioException;
+import nl.openweb.iot.wio.db.GroveBean;
+import nl.openweb.iot.wio.domain.Grove;
+import nl.openweb.iot.wio.domain.Node;
+import nl.openweb.iot.wio.domain.Type;
+import nl.openweb.iot.wio.rest.NodeResource;
+
+@Type
+public class GroveUltraRanger extends Grove {
+    public GroveUltraRanger(GroveBean groveBean, Node parent, NodeResource nodeResource) {
+        super(groveBean, parent, nodeResource);
+    }
+
+    public Double readRangeInCm() throws WioException {
+        return toDouble(readSimpleProperty("range_in_cm", "range_cm"));
+    }
+
+    public Double readRangeInInch() throws WioException {
+        return toDouble(readSimpleProperty("range_in_inch", "range_inch"));
+    }
+}
