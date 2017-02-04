f58d02e4ca880f063b7c3f0b0324812bb226e90a
Ebrahim aharpour <ebrahim@openweb.nl>
2017-02-04 15:22:43 +0100

    Simplifying core demo

--- demo/core-demo/src/main/java/nl/openweb/iot/demo/Runner.java
+++ demo/core-demo/src/main/java/nl/openweb/iot/demo/Runner.java
@@ -1,14 +1,11 @@
 package nl.openweb.iot.demo;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import nl.openweb.iot.demo.domain.Reading;
-import nl.openweb.iot.demo.repository.ReadingRepository;
 import nl.openweb.iot.wio.domain.grove.GroveTempHumPro;
+import nl.openweb.iot.wio.scheduling.ScheduledTask;
 import nl.openweb.iot.wio.scheduling.SchedulingService;
 import nl.openweb.iot.wio.scheduling.SchedulingUtils;
 
@@ -25,10 +22,11 @@ public class Runner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         schedulingService.build("Ebi01", (node, context) -> {
-            ReadingRepository repository = context.getBean(ReadingRepository.class);
+            ScheduledTask.TaskExecutionResult taskExecutionResult = SchedulingUtils.minutesLater(5);
             GroveTempHumPro temp = node.getGroveByType(GroveTempHumPro.class).get();
-            repository.save(new Reading(temp.readHumidity(), temp.readTemperature(), new Date()));
-            return SchedulingUtils.minutesLater(5);
+            System.out.println("Temperature: " + temp.readTemperature());
+            System.out.println("Humidity: " + temp.readHumidity());
+            return taskExecutionResult;
         }).setEventHandler((e, n, c) -> System.out.println(e)).setForceSleep(false).build();
     }
 }
--- demo/core-demo/src/main/java/nl/openweb/iot/demo/repository/ReadingRepository.java
+++
@@ -1,8 +0,0 @@
-package nl.openweb.iot.demo.repository;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import nl.openweb.iot.demo.domain.Reading;
-
-public interface ReadingRepository extends ElasticsearchRepository<Reading, String> {
-}
