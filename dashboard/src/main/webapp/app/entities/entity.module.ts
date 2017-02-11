d161b764d5aee122fe1e94b0180b1b961157b689
Ebrahim aharpour <ebrahim@openweb.nl>
2017-02-11 02:47:44 +0100

    Revert "entities generated."
    This reverts commit e3a9efdac4023bdcf543597cb4aa11b79cf59e49.

--- dashboard/src/main/webapp/app/entities/entity.module.ts
+++ dashboard/src/main/webapp/app/entities/entity.module.ts
@@ -1,17 +1,9 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { DashboardEventHandlerModule } from './event-handler/event-handler.module';
-import { DashboardNodeModule } from './node/node.module';
-import { DashboardTaskModule } from './task/task.module';
-import { DashboardTaskHandlerModule } from './task-handler/task-handler.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
-        DashboardEventHandlerModule,
-        DashboardNodeModule,
-        DashboardTaskModule,
-        DashboardTaskHandlerModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
--- dashboard/src/main/webapp/app/entities/event-handler/event-handler.model.ts
+++
@@ -1,7 +0,0 @@
-export class EventHandler {
-    constructor(
-        public id?: number,
-        public name?: string,
-        public className?: string,
-    ) { }
-}
